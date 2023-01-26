2b3fa80f17710f4607cac3f6eb472e1ade2ba143
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-01-26 16:11:07 +0700

    email_signup

---
+++ data.js
@@ -0,0 +1,67 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+import { getDatabase, ref, set, child, update, remove, onValue } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBkFFG0G5Pf7kpj8Te3yL8w9b7p6XMinxs",
+    authDomain: "signin-b4395.firebaseapp.com",
+    databaseURL: "https://signin-b4395-default-rtdb.firebaseio.com",
+    projectId: "signin-b4395",
+    storageBucket: "signin-b4395.appspot.com",
+    messagingSenderId: "291490998392",
+    appId: "1:291490998392:web:00db15797aec1ce588658d"
+};
+const app = initializeApp(firebaseConfig);
+
+function write_db() {
+    console.log("Working");
+    var db = getDatabase();
+    var create_db_table = ref(db, 'users/' + 'usersTable/');
+    var user_name = document.getElementById("email_signup").value;
+    var user_password = document.getElementById("password_signup").value;
+    if (user_name == '' || user_password == '') {
+        alert("Please fill out all fields");
+        console.log("Please fill out all fields");
+        throw "Please fill out all fields";
+    }
+    set(ref(db, 'users/' + 'usersTable/'), {
+        user_name: user_name,
+        user_name_password: user_password
+    }).then((res) => {
+        console.log();
+    })
+        .catch((err) => {
+            alert(err.message);
+            console.log(err.code);
+            console.log(err.message);
+        })
+}
+
+// Read
+function read_db() {
+    var db = getDatabase();
+    var connect_db = ref(db, 'users/' + 'usersTable/');
+    var retrieve_data = '';
+    console.log("Reading");
+    onValue(connect_db, (snapshot) => {
+        retrieve_data = snapshot.val();
+        console.log("user_name: " + retrieve_data.user_name);
+        call_loop_print(retrieve_data);
+        document.getElementById("display_read_data").innerHTML = "<pre>" + "user_name: " + retrieve_data.user_name + "</pre>" +
+            '\n' + "<pre>" + "user_name_password: " + retrieve_data.user_name_password + "</pre>";
+    })
+    function call_loop_print(retrieve_data) {
+        for (var r = 0; r < Object.entries(retrieve_data).length; r++) {
+            var key = Object.keys(retrieve_data)[r];
+            var value = retrieve_data[key];
+            console.log("Key_" + r + ': ' + key + " Value_:" + r + ': ' + value);
+        }
+    }
+}
+// Call
+var write_data_to_firebase = document.getElementById("create-acct-btn");
+write_data_to_firebase.addEventListener('click', write_db);
+
+// Call
+var read_data_from_firebase = document.getElementById("read_data_from_firebase");
+read_data_from_firebase.addEventListener('click', read_db);
--- script.js
+++ script.js
@@ -28,17 +28,14 @@ const passwordInput = document.getElementById("password");
 const main = document.getElementById("main");
 const createacct = document.getElementById("create-acct")
 
-const signupEmailIn = document.getElementById("email-signup");
+const signupEmailIn = document.getElementById("email_signup");
 const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
-const signupPasswordIn = document.getElementById("password-signup");
-// const confirmSignUpPasswordIn = document.getElementById("confirm-password-signup");
+const signupPasswordIn = document.getElementById("password_signup");
 const createacctbtn = document.getElementById("create-acct-btn");
 
 const returnBtn = document.getElementById("return-btn");
 
 var email, password, signupEmail, signupPassword, confirmSignupEmail;
-
-
 
 
 
@@ -53,31 +50,25 @@ createacctbtn.addEventListener("click", function () {
     }
 
     signupPassword = signupPasswordIn.value;
-    // confirmSignUpPassword = confirmSignUpPasswordIn.value;
-    // if (signupPassword ) {
-    //     window.alert("Password fields do not match. Try again.")
-    //     isVerified = false;
-    // }
-
     if (signupEmail == null || confirmSignupEmail == null || signupPassword == null) {
-        window.alert("Please fill out all required fields.");
+        window.alert("Please fill out all fields.");
         isVerified = false;
     }
 
     if (isVerified) {
         createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
             .then((userCredential) => {
-                // Signed in 
+              // Sign up 
                 const user = userCredential.user;
                 localStorage.setItem('signupEmail', JSON.stringify(user));
-                // ...
-                window.alert("Success! Account created.");
+
+                window.alert("Signed up!");
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
-                // ..
-                window.alert("Error occurred. Try again.");
+
+                window.alert("Try again.");
             });
     }
 });
@@ -90,17 +81,16 @@ submitButton.addEventListener("click", function () {
 
     signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            // Signed in
+            // Sign in
             const user = userCredential.user;
-            console.log("Success! Welcome back!");
-            window.alert("Success! Welcome back!");
-            // ...
+            console.log("Welcome back!");
+            window.alert("Welcome back!");
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            console.log("Error occurred. Try again.");
-            window.alert("Error occurred. Try again.");
+            console.log("Try again.");
+            window.alert("Try again.");
         });
 });
 
@@ -113,59 +103,3 @@ returnBtn.addEventListener("click", function () {
     main.style.display = "block";
     createacct.style.display = "none";
 });
-
-
-function write_db() {
-    console.log("DEBUG: Write function");
-    var db = getDatabase();
-    var create_db_table = ref(db, 'ESP32_DB/' + 'ESP32_User_Login_Table/');
-    var user_name = document.getElementById("user_name").value;
-    var user_password =  document.getElementById("user_name_password").value;
-    if( user_name == '' || user_password == ''){
-        alert("Make sure, must be non-empty data is required!!!");
-        console.log("Make sure, must be non-empty data is required!!!");
-        throw "Make sure, must be non-empty data is required!!!";
-    }
-    set(ref(db, 'ESP32_DB/' + 'ESP32_User_Login_Table/'), {
-      user_name: user_name,
-      user_name_password: user_password
-    }).then((res) => {
-        console.log();
-    })
-    .catch((err) => {
-        alert(err.message);
-        console.log(err.code);
-        console.log(err.message);
-    })
-}
-
-/***** read data from firebase *****/
-function read_db() {
-    var db = getDatabase();
-    var connect_db = ref(db, 'ESP32_DB/' + 'ESP32_User_Login_Table/');
-    var retrieve_data='';
-    console.log("DEBUG: Read function");
-    onValue(connect_db, (snapshot) => {
-        retrieve_data = snapshot.val();
-        //console.log("user_name: " + retrieve_data.user_name);
-        //console.log("user_name_password: " + retrieve_data.user_name_password);
-        call_loop_print(retrieve_data);
-        document.getElementById("display_read_data").innerHTML =  "<pre>" + "user_name: " + retrieve_data.user_name +
-                '\n' + "user_name_password: " + retrieve_data.user_name_password + "</pre>";
-        })
-    function call_loop_print(retrieve_data){
-        for (var r=0;r<Object.entries(retrieve_data).length;r++){
-            var key = Object.keys(retrieve_data)[r];
-            var value = retrieve_data[key];
-            console.log("Key_" + r + ': ' + key + " Value_:" + r + ': ' + value );
-           }
- }
-}
-/***** call write data function *****/
-var write_data_to_firebase = document.getElementById("write_data_to_firebase");
-write_data_to_firebase.addEventListener('click', write_db);
-
-/***** call read data function *****/
-var read_data_from_firebase = document.getElementById("read_data_from_firebase");
-read_data_from_firebase.addEventListener('click', read_db);
-
